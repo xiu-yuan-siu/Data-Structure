e1051b308637aef49ea10879314864e48a58c1b2
xiu-yuan-siu <yuanxiu_0903@qq.com>
2026-03-26 10:59:45 +0000

    Linked Stack

--- Chapter_3 Stack and Queue/3.1. Stack/2. Linked Stack/2.1 Linked Stack.c
+++ Chapter_3 Stack and Queue/3.1. Stack/2. Linked Stack/2.1 Linked Stack.c
@@ -1,2 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../../Status.h"
+
+typedef int SElemType;
+
+typedef struct StackNode
+{
+    SElemType data;
+    struct StackNOde *next;
+}StackNode, *LinkStack;
+
+Status InitStack(LinkStack S)
+{
+    S = NULL;
+    S->next = NULL;
+    return OK;
+}
+
+Status GetTop(LinkStack S, SElemType *e)
+{
+    if (S == NULL)
+    {
+        return ERROR;
+    }
+    *e = S->data;
+    return OK;
+}
+
+Status Push(LinkStack S, SElemType e)
+{
+    StackNode *p = (StackNode*)malloc(sizeof(StackNode));
+    p->data = e;
+    p->next = S;
+    S = p;  // 更新头指针
+    return OK;
+}
+
+Status Pop(LinkStack S, SElemType *e)
+{
+    if (S == NULL)  // 空栈
+    {
+        return ERROR;
+    }
+    StackNode *p = S;
+    *e = S->data;
+    S = S->next;
+    free(p);
+    return OK;
+}
+
+Status DestroyStack(LinkStack S)
+{
+    StackNode *p = S;
+    while (p)
+    {
+        StackNode *q = p;
+        free(q);
+        p = p->next;
+    }
+    // 循环结束后 p 已经为 NULL，不需要单独释放
+    free(S);  // 释放头指针
+    return OK;
+}
+
+Status ClearStack(LinkStack S)
+{
+    StackNode *p = S;
+    while (p)
+    {
+        StackNode *q = p;
+        free(q);
+        p = p->next;
+    }
+    return OK;
+}
+
+Status StackEmpty(LinkStack S)
+{
+    if (S->next == NULL)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
--- Chapter_3 Stack and Queue/3.1. Stack/2. Linked Stack/2.2 Linked Stack.cpp
+++ Chapter_3 Stack and Queue/3.1. Stack/2. Linked Stack/2.2 Linked Stack.cpp
@@ -1,3 +1,75 @@
 #include <iostream>
 #include <stdexcept>
 using namespace std;
+
+template <class SElemType>
+class LinkStack
+{
+private:
+    struct StackNode 
+    {
+        SElemType data;
+        StackNode *next;
+        StackNode (SElemType d = SElemType(), StackNode *n = nullptr) : data(d), next(nullptr) {};  // 默认列表初始化
+    };
+    StackNode *head;  // 头指针
+public:
+    // 默认初始化
+    LinkStack()
+    {
+        head = new StackNode();
+        if (!head)
+        {
+            throw bad_alloc("内存分配失败");
+        }
+    }
+    // 析构函数
+    ~LinkStack()
+    {
+        this->clear();
+        delete head;
+        head = nullptr;
+    }
+    void clear()
+    {
+        StackNode *p = head;
+        while (p)
+        {
+            StackNode *q = p;
+            delete q;
+            p = p->next;
+        }
+    }
+    SElemType getTop()
+    {
+        if (head == nullptr)
+        {
+            throw runtime_error("栈为空，无法获取栈顶元素");
+        }
+        return head->data;
+    }
+    void push(SElemType &e)
+    {
+        StackNode *p = new StackNode();
+        p->data = e;
+        p->next = head;
+        head = p;
+    }
+    SElemType pop()
+    {
+        if (head == nullptr)
+        {
+            throw runtime_error("栈为空，无法删除栈顶元素");
+        }
+        StackNode *p = S;
+        S = S->next;
+        SElemType temp = p->data;
+        delete p;
+        p = nullptr;
+        return temp;
+    }
+    bool empty()
+    {
+        return head == nullptr;
+    }
+};
